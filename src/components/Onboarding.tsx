e84b3f38f21dae7f48d59552a24c9a467ad5cfcd
kihyvari <kihyvari@gmail.com>
2020-10-29 13:15:10 +0200

    Tries to spam flex-box and something else.

--- src/components/Onboarding.tsx
+++ src/components/Onboarding.tsx
@@ -7,34 +7,33 @@ const Onboarding: React.FC = () => {
   return (
     <IonContent className="onboarding">
       <IonSlides pager className="parentSlides">
-        <IonSlide className="childrenSlide"> {/** TODO remove or use */}
+        <IonSlide className="childrenSlide">
+          <h2 className="onboardingHeader">Löydä koulutuksesi</h2>
           <div className="diag">
             <IonIcon icon={americanFootball} color="primary"></IonIcon>
           </div>
-          <h2 className="onboardingHeader">Löydä koulutuksesi</h2>
-          <p className="onboardingParagraph">Löydä pipopään & pöllön avustuksella korkeakoulusi!</p> {/** football  */}
+          <p className="onboardingParagraph">Löydä pipopään & pöllön avustuksella korkeakoulusi!</p>
         </IonSlide>
 
-        <IonSlide>
+        <IonSlide className="childrenSlide">
+          <h2 className="onboardingHeader">Ratkaise tehtävät</h2>
           <div className="diag">
             <IonIcon icon={medal} color="primary"></IonIcon>
           </div>
-          <h2 className="onboardingHeader">Ratkaise tehtävät</h2>
-          <p className="onboardingParagraph">Pelaa kaupungit läpi ja pohdi vastauksia</p>{' '}
-          {/** medal */}
+          <p className="onboardingParagraph">Pelaa kaupungit läpi ja pohdi vastauksia</p>
         </IonSlide>
 
-        <IonSlide>
+        <IonSlide className="childrenSlide">
+          <h2 className="onboardingHeader">Pohdi lopputulosta</h2>
           <div className="diag">
             <IonIcon icon={analyticsOutline} color="primary"></IonIcon>
           </div>
-          <h2 className="onboardingHeader">Pohdi lopputulosta</h2>
-          <p className="onboardingParagraph">Vastauksien perusteella saat lopputulosteen!</p> {/** analytics */}
+          <p className="onboardingParagraph">Vastauksien perusteella saat lopputulosteen!</p>
         </IonSlide>
       </IonSlides>
       <IonButton shape="round" id="skip">
         Skip this!
-      </IonButton>{' '}
+      </IonButton>
       {/** skip-function here */}
     </IonContent>
   )
