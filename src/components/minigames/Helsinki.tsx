7a23bd72b48137404cbdd583ceff553ba626e06e
Aleksi134 <aleleskinen11@gmail.com>
2020-11-03 14:07:30 +0200

    datafunktioita

--- src/components/minigames/Helsinki.tsx
+++ src/components/minigames/Helsinki.tsx
@@ -1,4 +1,14 @@
-import { IonButton, IonCard, IonCardContent, IonInput, IonItem, IonItemDivider, IonLabel, IonList } from '@ionic/react'
+import {
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonInput,
+  IonItem,
+  IonItemDivider,
+  IonLabel,
+  IonList,
+} from '@ionic/react'
+import { isNumber } from 'lodash'
 import React, { useState } from 'react'
 import { Result } from '../../GameState'
 import './Minigame.css'
@@ -11,76 +21,108 @@ interface Props {
 const initialState = { text: '', number: null }
 
 const MiniGame: React.FC<Props> = ({ state = initialState, done }) => {
-  const [text, setText] = useState<string>(state.text)
-  const [number, setNumber] = useState<number>(state.number)
+  const [text, setText] = useState({
+    text1: '',
+    text2: '',
+    text3: '',
+    text4: '',
+  })
+  const [number, setNumber] = useState({
+    number1: Number,
+  })
 
-  const saveAndClose = () => done({text, number})
+  const saveAndClose = () => done({ text, number })
+
+  const [formData, setFormData] = useState([])
+
+  const onInputChange = () => {
+    let copyFormData = [...formData] as any
+    copyFormData.push({ 'text from field': text, 'numbers from field': number })
+    console.log(copyFormData)
+    setFormData(copyFormData)
+  }
+
+  const saveOnClick = () => {
+    onInputChange()
+    saveAndClose()
+  }
 
   return (
-      <div className="container minigame">
+    <div className="container minigame">
+      <IonCard>
+        <IonItem>
+          <img
+            style={{ width: '60px' }}
+            src="assets/kuunteleva.png"
+            alt="kuunteleva"
+            slot="start"
+          />
+          <IonLabel>Formeja alla</IonLabel>
+        </IonItem>
 
-        <IonCard>
-          <IonItem>
-            <img style={{ width: '60px' }} src="assets/kuunteleva.png" alt="kuunteleva" slot="start" />
-            <IonLabel>Formeja alla</IonLabel>
-          </IonItem>
+        <IonCardContent>
+          <p>Alta löytyy täytettäviä formeja.</p>
+        </IonCardContent>
+      </IonCard>
+      <IonList>
+        <IonItemDivider>Input with clear button when there is a value</IonItemDivider>
+        <IonItem>
+          <IonInput
+            value={text.text1}
+            placeholder="Enter Input"
+            onIonChange={(e) => setText((prev) => ({ ...prev, text1: e.detail.value! }))}
+            clearInput
+            required
+          ></IonInput>
+        </IonItem>
 
-          <IonCardContent>
-            <p>Alta löytyy täytettäviä formeja.</p>
-          </IonCardContent>
-        </IonCard>
+        <IonItemDivider>Input with clear button when there is a value</IonItemDivider>
+        <IonItem>
+          <IonInput
+            value={text.text2}
+            placeholder="Enter Input"
+            onIonChange={(e) => setText((prev) => ({ ...prev, text2: e.detail.value! }))}
+            clearInput
+            required
+          ></IonInput>
+        </IonItem>
 
-        <IonList>
-          <IonItemDivider>Default Input with Placeholder</IonItemDivider>
-          <IonItem>
-            <IonInput value={text} placeholder="Enter Input" onIonChange={e => setText(e.detail.value!)}></IonInput>
-          </IonItem>
+        <IonItemDivider>Input with clear button when there is a value</IonItemDivider>
+        <IonItem>
+          <IonInput
+            value={text.text3}
+            placeholder="Enter Input"
+            onIonChange={(e) => setText((prev) => ({ ...prev, text3: e.detail.value! }))}
+            clearInput
+            required
+          ></IonInput>
+        </IonItem>
 
-          <IonItemDivider>Input with clear button when there is a value</IonItemDivider>
-          <IonItem>
-            <IonInput value={text} placeholder="Enter Input" onIonChange={e => setText(e.detail.value!)} clearInput></IonInput>
-          </IonItem>
+        <IonItemDivider>Input with clear button when there is a value</IonItemDivider>
+        <IonItem>
+          <IonInput
+            value={text.text4}
+            placeholder="Enter Input"
+            onIonChange={(e) => setText((prev) => ({ ...prev, text4: e.detail.value! }))}
+            clearInput
+            required
+          ></IonInput>
+        </IonItem>
 
-          <IonItemDivider>Number type input</IonItemDivider>
-          <IonItem>
-            <IonInput type="number" value={number} placeholder="Enter Number" onIonChange={e => setNumber(parseInt(e.detail.value!, 10))}></IonInput>
-          </IonItem>
-
-          <IonItemDivider>Disabled input</IonItemDivider>
-          <IonItem>
-            <IonInput value={text} disabled></IonInput>
-          </IonItem>
-
-          <IonItemDivider>Readonly input</IonItemDivider>
-          <IonItem>
-            <IonInput value={text} readonly></IonInput>
-          </IonItem>
-
-          <IonItemDivider>Inputs with labels</IonItemDivider>
-
-          <IonItem>
-            <IonLabel>Default Label</IonLabel>
-            <IonInput></IonInput>
-          </IonItem>
-
-          <IonItem>
-            <IonLabel position="floating">Floating Label</IonLabel>
-            <IonInput value={text}></IonInput>
-          </IonItem>
-
-          <IonItem>
-            <IonLabel position="fixed">Fixed Label</IonLabel>
-            <IonInput value={text}></IonInput>
-          </IonItem>
-
-          <IonItem>
-            <IonLabel position="stacked">Stacked Label</IonLabel>
-            <IonInput value={text}> </IonInput>
-          </IonItem>
-        </IonList>
-
-        <IonButton className="done" onClick={saveAndClose}>Merkitse suoritetuksi</IonButton>
-      </div>
+        <IonItemDivider>Number type input</IonItemDivider>
+        <IonItem>
+          <IonInput
+            value={number.number1}
+            type="number"
+            placeholder="Enter Number"
+          onIonChange={(e) => setNumber(parseInt(e.detail.value!, 10))}
+          ></IonInput>
+        </IonItem>
+      </IonList>
+      <IonButton className="done" onClick={saveOnClick}>
+        Merkitse suoritetuksi
+      </IonButton>
+    </div>
   )
 }
 
