8c7f5e3f58b5e74589c122e4c7030ec47a51f6ff
Perttu Iso-Metsälä <perttu.im@gmail.com>
2020-11-10 16:28:11 +0200

    Uusi svg progressiolle

--- src/components/ProgressMeter.tsx
+++ src/components/ProgressMeter.tsx
@@ -67,28 +67,28 @@ const ProgressMeter: React.FC<Props> = ({ history }) => {
   return (
     <div onClick={navigateToResults} className="progress-meter">
 
-      <svg ref={svgRef} version="1.1" viewBox="0 0 71.549 71.7" xmlns="http://www.w3.org/2000/svg">
+      <svg ref={svgRef} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 271 271">
         <defs>
-          <style>{`.cls-1{fill:#e8e8e8;stroke:#d3d3d3;}.cls-1,.cls-2{stroke-miterlimit:10;}.cls-2{fill:#2bb9cc;stroke:#000;}`}</style>
+          <style>{`.cls-1{fill:#e8e8e8;stroke:#d3d3d3;}.cls-1,.cls-2{stroke - miterlimit:10;}.cls-2{fill:#2bb9cc;stroke:#000;}`}</style>
         </defs>
-        <g transform="translate(-97.188 -91.906)">
-          <g id="progress" transform="matrix(.26458 0 0 .26458 97.189 91.906)">
-            <path ref={refs.itsetuntemus[1]} className="cls-1 active" d="m135.3 135.51 79.34-109.38a135 135 0 0 0-158.82 0.39l79.35 109z" />
-            <path ref={refs.itsetuntemus[0]} className="cls-1" d="m133.81 137.02 59.505-82.035a101.25 101.25 0 0 0-119.12 0.2925l59.512 81.75z" />
+        <g id="Layer_2" data-name="Layer 2">
+          <g id="progress">
+            <path ref={refs.itsetuntemus[1]} className="cls-1" d="M135,37a98,98,0,0,1,58,18.91l21.61-29.78a135,135,0,0,0-158.82.39L77.12,55.8A98.06,98.06,0,0,1,135,37Z" />
+            <path ref={refs.itsetuntemus[0]} className="cls-1" d="M77.12,55.8l58,79.75.13,0L193,55.91A98.5,98.5,0,0,0,77.12,55.8Z" />
 
-            <path ref={refs.tyoelamatietous[1]} className="cls-1" d="m135 135.5v0.11l0.17-0.06-79.35-109a135.13 135.13 0 0 0-48.72 150.73l127.9-41.67z" />
-            <path ref={refs.tyoelamatietous[0]} className="cls-1" d="m133.58 137.01v0.0825l0.1275-0.045-59.512-81.75a101.35 101.35 0 0 0-36.54 113.05l95.925-31.252z" />
+            <path ref={refs.tyoelamatietous[1]} className="cls-1" d="M36.5,135.5A98.36,98.36,0,0,1,77.12,55.8L55.82,26.52A135.13,135.13,0,0,0,7.1,177.28l34.26-11.17A98.3,98.3,0,0,1,36.5,135.5Z" />
+            <path ref={refs.tyoelamatietous[0]} className="cls-1" d="M135,135.5v.11l.17-.06-58-79.75A98.58,98.58,0,0,0,41.36,166.11L135,135.61Z" />
 
-            <path ref={refs.tietojaopiskelusta[1]} className="cls-1" d="m134.94 136 0.06-0.08v-0.31l-127.9 41.67a135 135 0 0 0 127.9 93.21v-134.57z" />
-            <path ref={refs.tietojaopiskelusta[0]} className="cls-1" d="m133.54 137.39 0.045-0.06v-0.2325l-95.925 31.252a101.25 101.25 0 0 0 95.925 69.908v-100.93z" />
+            <path ref={refs.tietojaopiskelusta[1]} className="cls-1" d="M41.36,166.11,7.1,177.28A135,135,0,0,0,135,270.49V234A98.53,98.53,0,0,1,41.36,166.11Z" />
+            <path ref={refs.tietojaopiskelusta[0]} className="cls-1" d="M134.94,136l.06-.08v-.31l-93.64,30.5A98.53,98.53,0,0,0,135,234V135.92Z" />
 
-            <path ref={refs.elamantilanne[1]} className="cls-1" d="m136.53 134.04 126.94 41.137a133.31 133.31 0 0 0-48.832-149.04l-78.157 107.79z" />
-            <path ref={refs.elamantilanne[0]} className="cls-1" d="m134.73 135.92 95.204 30.852a99.986 99.986 0 0 0-36.624-111.78l-58.618 80.84z" />
+            <path ref={refs.elamantilanne[1]} className="cls-1" d="M214.64,26.13,193,55.91a98.59,98.59,0,0,1,35.74,109.81L264,177.12a135.12,135.12,0,0,0-49.31-151Z" />
+            <path ref={refs.elamantilanne[0]} className="cls-1" d="M135.93,135.19l-.09,0-.19.26,93.68,30.4a98.55,98.55,0,0,0-35.81-110l-57.68,79.32Z" />
 
-            <path ref={refs.valintojentekeminen[1]} className="cls-1" d="m136.71 134.23 0.10324 133.44a133.31 133.31 0 0 0 126.66-92.498l-126.66-41.024z" />
-            <path ref={refs.valintojentekeminen[0]} className="cls-1" d="m134.87 136.06 0.0774 100.08a99.986 99.986 0 0 0 94.993-69.374l-94.997-30.768z"  />
+            <path ref={refs.valintojentekeminen[1]} className="cls-1" d="M135,234v36.49h.5A135.06,135.06,0,0,0,264,177.12l-35.18-11.4A98.53,98.53,0,0,1,135,234Z" />
+            <path ref={refs.valintojentekeminen[0]} className="cls-1" d="M136.15,135.38l-.06-.08-.3-.1-.2,98.49a98.54,98.54,0,0,0,93.69-67.82L136.09,135.3Z" />
 
-            <circle className="cls-2" cx="135" cy="135.5" r="61.5" />
+            <circle className="cls-2" cx="133" cy="135.5" r="61.5" />
           </g>
         </g>
       </svg>
